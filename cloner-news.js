dfa0665ba71c1abbd46303cd2d5d680ed3e28b4f
jwambugu <wjoanmumbi@gmail.com>
2024-09-15 23:27:59 +0300

    chore: response error handling

--- cloner-news.js
+++ cloner-news.js
@@ -44,6 +44,9 @@ function showPopup(message) {
 async function fetchStories(storyType) {
     try {
         const response = await fetch(`https://hacker-news.firebaseio.com/v0/${storyType}.json`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
         const data = await response.json();
         allStories = data;
         if (storyType === 'newstories') {
